b9e079b528f383be2ab2713a2c610af53369e6fb
Molitvan <82037155+Molitvan@users.noreply.github.com>
2026-06-12 13:55:24 +0200

    Change tabs to spaces

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+  import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head>
-<title>Darkade</title>
-	<link rel="icon" href={favicon} />
+  <title>Darkade</title>
+  <link rel="icon" href={favicon} />
 </svelte:head>
 
 {@render children()}
